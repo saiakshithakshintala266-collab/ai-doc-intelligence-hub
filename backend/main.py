d623811a2823772f8acb495ff5bf88df28b1fa7d
Sai Akshith Akshintala <saiakshithakshintala266@gmail.com>
2026-02-07 16:30:00 -0600

    Fix CORS to allow CloudFront frontend

--- backend/main.py
+++ backend/main.py
@@ -10,7 +10,7 @@ app = FastAPI(
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["https://d2coveoak3zniz.cloudfront.net"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
